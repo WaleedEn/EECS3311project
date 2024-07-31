b74970aebd672c4b912674d16a6340259e3a2bb9
ahnafR001 <ahnaf001@my.yorku.ca>
2024-07-31 16:13:07 -0400

    Updated App.java and created the class APIHandler class for the url and server context

---
+++ project/src/main/java/ca/yorku/eecs/APIHandler.java
@@ -0,0 +1,27 @@
+package ca.yorku.eecs;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class APIHandler implements HttpHandler {
+
+	@Override
+	public void handle(HttpExchange arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+		int status_code;
+		String response_line; 
+		
+		
+		
+		//getting a path and method from the requested url
+		
+		
+		
+		
+		
+	}
+
+}
