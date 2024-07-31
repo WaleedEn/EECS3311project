510677cd6b41c9fa7cf0d33c857a1d7dc6664830
Lucas Cajamarca Verdugo <lucasverdugo@outlook.com>
2024-07-31 15:56:18 -0400

    Added Neo4jConfig Class
    This class connects the project to the database.

---
+++ project/src/main/java/ca/yorku/eecs/Neo4jConfig.java
@@ -0,0 +1,27 @@
+package ca.yorku.eecs;
+
+import static org.neo4j.driver.v1.Values.parameters;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Config;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.Transaction;
+
+public class Neo4jConfig implements AutoCloseable{
+    private Driver driver;
+    private final String uriDb = "bolt://localhost:7687";
+    private final String USERNAME = "neo4j";
+    private final String PASSWORD = "12345678";
+
+    public Neo4jConfig(){
+        Config config = Config.builder().withoutEncryption().build();
+        driver = GraphDatabase.driver(uriDb, AuthTokens.basic(USERNAME, PASSWORD), config);
+    }
+
+    @Override
+    public void close() throws Exception {
+        driver.close();
+    }
+}
